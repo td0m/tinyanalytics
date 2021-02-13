4795aa4fdab930bade4f3c88d99c7129f7b52eec
Dominik Tarnowski <dom@tdom.dev>
2021-02-13 23:44:03 +0000

    Add basic user validation

--- user.go
+++ user.go
@@ -1,7 +1,33 @@
 package model
 
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
+// errors
+var (
+	ErrPassTooShort = errors.New("user password is too short")
+	ErrInvalidEmail = errors.New("email seems to be in an invalid format")
+)
+
+var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
 // User model
 type User struct {
-	Email string
-	Pass  string
+	Email string `json:"email,omitempty"`
+	Pass  string `json:"-"`
+}
+
+// Validate returns an error if the user is not valid
+func (u *User) Validate() error {
+	u.Email = strings.ToLower(u.Email)
+	if len(u.Pass) < 8 {
+		return ErrPassTooShort
+	}
+	if !emailRegex.MatchString(u.Email) {
+		return ErrInvalidEmail
+	}
+	return nil
 }
